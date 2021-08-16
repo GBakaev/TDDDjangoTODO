b915e89e20a1c00c581a47be19bc6ac98ce0e247
George <gebakaev@hotmail.com>
2021-08-16 20:13:03 +0300

    Wrote Functional tests for adding a new item.

--- funtional_test.py
+++ funtional_test.py
@@ -1,25 +1,55 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
 import os
 
-# Get Current Dir Path
-currentDir = os.path.dirname(os.path.abspath(__file__))
+# New Visitor Tests
+class NewVisisorTest(unittest.TestCase):
+    # Runs on setup
+    def setUp(self): 
+        # Get Current Dir Path
+        self.currentDir = os.path.dirname(os.path.abspath(__file__))
+        # Create ChromeDriver and Fetch Local Website
+        self.browser = webdriver.Chrome(self.currentDir + "/chromedriver")
 
-# Create ChromeDriver and Fetch Local Website
-browser = webdriver.Chrome(currentDir + "/chromedriver")
-browser.get('http://localhost:8000')
+    # Runs at the end
+    def tearDown(self):
+        # Quit Driver
+        self.browser.quit()
 
-# We check the page title that is: Awesome Lists
-assert 'Awesome Lists' in browser.title
+    # Test user can start a list and save it
+    def test_user_can_start_new_list(self):
+        # Get the Webpage
+        self.browser.get('http://localhost:8000')
 
-# We want to enter a TO-DO Item
+        # We check the page title that is: Awesome Lists
+        assert 'Awesome Lists' in self.browser.title
 
-# We enter 'Do Homework' in the textfield
+        # We want to enter a TO-DO Item
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputBox.get_attribute('placeholder'),
+            'Enter a TO-Do item'
+        )
+        
+        # We enter 'Do Homework' in the textfield
+        inputBox.send_keys('Do Homework')
 
-# We hit enter, the page gets refreshed and we see our To-Do item there
+        # We hit enter, the page gets refreshed and we see our To-Do item there
+        inputBox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Do Homework' for row in rows)
+        )
 
-# We add another item, this time 'Study for TDD'
+        # Finish the tests
+        self.fail('Finish the Tests.')
 
-# The page refreshes again and we see our updated TO-Do list
+        # We add another item, this time 'Study for TDD'
 
-# Quit Driver
-browser.quit()
+        # The page refreshes again and we see our updated TO-Do list
+
+# Run all the functional tests.
+if __name__ == '__main__':
+    unittest.main()
